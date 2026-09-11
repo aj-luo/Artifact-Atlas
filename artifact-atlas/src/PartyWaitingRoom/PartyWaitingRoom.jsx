c1928cf78f86cc285c3ae70b13dd1d19dfa69e62
Albert Luo <aj.luo@mail.utoronto.ca>
2026-09-10 22:18:27 -0400

    made lobby functionality

---
+++ artifact-atlas/src/PartyWaitingRoom/PartyWaitingRoom.jsx
@@ -0,0 +1,34 @@
+import styles from './PartyWaitingRoom.module.css';
+import { useState } from 'react';
+
+function PartyWaitingRoom({ setCurrentView, gameId }) {
+    //These are the default values for time limit and player count. They can be changed by the user using the toggles below. hooks can be used to store the values and update them when the user changes them. The values can then be passed to the backend when creating the lobby.
+    const [playerCount, setPlayerCount] = useState(4);
+    const [timeLimit, setTimeLimit] = useState(5);
+    const [lobbyFull, setLobbyFull] = useState(false); // Track if the lobby is full
+
+    return (
+        <div className={styles.home}>
+            {/* This is used to show the lobby status, whether we are waiting for players or lobby is full*/}
+            <p className={styles.tagline}>
+                {lobbyFull ? 'LOBBY IS FULL' : 'WAITING FOR OTHER PLAYERS'}
+            </p>
+
+            <p className={styles.tagline}>SHARE LOBBY ID: {gameId}</p>
+
+            <p className={styles.tagline}>PLAYERS JOINED:</p>
+
+            {/* Main actions container */}
+            <div className={styles.actionContainer}>
+                <button className={styles.start_button} onClick={() => setCurrentView('party')}>
+                    BACK
+                </button>
+                <button className={styles.start_button}>
+                    START GAME
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default PartyWaitingRoom;
